e7c19fb26f55916a7a896df1bcd572ea95e03be8
tssandor <Hello@sandor.im>
2020-08-13 15:33:19 +0800

    Data persistance reached

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,8 +4,7 @@ import Vapor
 public func configure(_ app: Application) throws {
   initializeWeightData()
   // uncomment to serve files from /Public folder
-  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
   // register routes
   try routes(app)
 }
--- Sources/App/weightData.swift
+++ Sources/App/weightData.swift
@@ -1,15 +1,31 @@
 var totalWeight: Float = 0
+let name = "weightDataFile"
 
 func initializeWeightData() {
   totalWeight = 0
   // if a backup exists, we load that
+  if let data = FileController.readFileSync(name), let content = String(data: data, encoding: .utf8) {
+    if let fromFileToFloat = Float(content) {
+      totalWeight = fromFileToFloat
+    }
+  }
+}
+
+func backupWeightData() -> Bool {
+  let weightToStr = String(totalWeight)
+  guard let data = weightToStr.data(using: .utf8)
+       else { return false }
+  FileController.writeFileSync(named: name, with: data)
+  return true
 }
 
 func newTotalWeight(_ weightToAdd: String) -> String {
   if let weightFloat = Float(weightToAdd) {
     totalWeight = totalWeight + weightFloat
   }
-  print(totalWeight)
+  if backupWeightData() {
+    print(totalWeight)
+  }
   return String(totalWeight)
 }
 
